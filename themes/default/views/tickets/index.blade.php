387ea6b8769c4d2b18ef7f886b6c4b159b5d2f93
Almesaody <hameralmesaody@gmail.com>
2023-05-16 07:46:02 +0300

    Updated ticket translation

--- themes/default/views/tickets/index.blade.php
+++ themes/default/views/tickets/index.blade.php
@@ -1,64 +1,78 @@
 <x-app-layout>
-    <x-slot name="title">
-        {{ __('Profile') }}
-    </x-slot>
-
-    <!-- show form to edit user profile -->
+    <!-- show open tickets -->
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg dark:bg-darkmode2">
-                <div class="p-6 sm:px-20 bg-white border-b border-gray-200 dark:bg-darkmode2 dark:border-black">
-                    <h1 class="dark:text-darkmodetext text-xl text-gray-500">Edit profile</h1>
-                    <div class="grid grid-cols-1 gap-4">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg dark:bg-darkmode2">
+                <div class="p-6 bg-white border-b border-gray-200 sm:px-20 dark:bg-darkmode2 dark:text-darkmodetext">
+                    <div class="mt-8 text-2xl dark:text-darkmodetext">
+                        {{ __('Open tickets') }}
+                    </div>
+                    <div class="mt-6 text-gray-500 dark:text-darkmodetext">
+                        {{ __('Open a ticket so you can talk to us.') }}
+                    </div>
                     <!-- display create ticket button -->
                     <div class="flex items-center justify-end mt-6">
                         <a href="{{ route('tickets.create') }}"
-                            class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
-                            {{ __('tickets.create_ticket') }}
+                            class="px-4 py-2 font-bold text-white bg-indigo-500 rounded hover:bg-indigo-700">
+                            {{ __('Create ticket') }}
                         </a>
                     </div>
-                        <x-success class="mt-4" />
-                        <div class="mt-6 text-gray-500 dark:text-darkmodetext dark:bg-darkmode2">
-                            <form method="POST" action="{{ route('profile.update') }}">
-                                @csrf
-                                <div class="mt-4">
-                                    <label for="name">{{ __('normal.name') }}</label>
-                                    <input id="name"
-                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm dark:bg-darkmode rounded-md"
-                                        name="enabled" required type="text" value="{{ Auth::user()->name }}">
+                    <div class="grid mt-6 text-gray-500 dark:text-darkmodetext sm:grid-cols-1 md:grid-cols-2">
+                        @if (count($tickets) > 0)
+                            <!-- show card per open ticket -->
+                            @foreach ($tickets as $ticket)
+                                <div class="grid grid-cols-1 m-2 bg-gray-200 bg-opacity-25 rounded-lg cursor-pointer dark:hover:shadow-lg dark:hover:bg-darkmodehover dark:transition dark:hover:transition dark:bg-darkmode" onclick="location.href='{{ route('tickets.show', $ticket->id) }}'">
+                                    <div class="p-6">
+                                        <div class="flex items-center">
+                                            <div class="ml-4 text-lg font-semibold leading-7 text-gray-600 dark:text-darkmodetext">
+                                                {{ $ticket->title }}
+                                            </div>
+                                        </div>
+                                        <div class="ml-12">
+                                            <div class="mt-2 text-sm text-gray-500 dark:text-darkmodetext">
+                                                {{ $ticket->description }}
+                                            </div>
+                                        </div>
+                                        <div class="ml-12">
+                                            <div class="mt-2 text-sm text-gray-500 dark:text-darkmodetext">
+                                                {{ __('Priority') }}:
+                                                @if ($ticket->priority == 'low')
+                                                    {{ __('Low Priority') }}
+                                                @elseif ($ticket->priority == 'medium')
+                                                    {{ __('Normal Priority') }}
+                                                @elseif ($ticket->priority == 'high')
+                                                    {{ __('High Priority') }}
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="ml-12">
+                                            <div class="mt-2 text-sm text-gray-500 dark:text-darkmodetext">
+                                                {{ __('Status') }}:
+                                                @if ($ticket->status == 'open')
+                                                    {{ __('open') }}
+                                                @elseif ($ticket->status == 'replied')
+                                                    {{ __('replied') }}
+                                                @elseif ($ticket->status == 'closed')
+                                                    {{ __('closed') }}
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <!-- last message -->
+                                        @if (count($ticket->messages) > 0)
+                                            <div class="ml-12">
+                                                <div class="mt-2 text-sm text-gray-500 dark:text-darkmodetext">
+                                                    {{ __('Last message') }}:
+                                                    {{ $ticket->messages->last()->message }}
+                                                </div>
+                                            </div>
+                                        @endif
+                                    </div>
                                 </div>
-                                <div class="mt-4">
-                                    <label for="address">{{ __('normal.address') }}</label>
-                                    <input id="address"
-                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm dark:bg-darkmode rounded-md"
-                                        name="address" required type="text" value="{{ Auth::user()->address }}">
-                                </div>
-                                <div class="mt-4">
-                                    <label for="city">{{ __('normal.city') }}</label>
-                                    <input id="city"
-                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm dark:bg-darkmode rounded-md"
-                                        name="city" required type="text" value="{{ Auth::user()->city }}">
-                                </div>
-                                <div class="mt-4">
-                                    <label for="country">{{ __('normal.country') }}</label>
-                                    <input id="country"
-                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm dark:bg-darkmode rounded-md"
-                                        name="country" required type="text" value="{{ Auth::user()->country }}">
-                                </div>
-                                <div class="mt-4">
-                                    <label for="phone">{{ __('normal.phone') }}</label>
-                                    <input id="phone"
-                                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm dark:bg-darkmode rounded-md"
-                                        name="phone" required type="text" value="{{ Auth::user()->phone }}">
-                                </div>
-                                <div class="flex items-center justify-end mt-4">
-                                    <button type="submit"
-                                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                        {{ __('Update') }}
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
+                            @endforeach
+
+                        @else
+                            {{ __('tickets.no_open_tickets') }}
+                        @endif
                     </div>
                 </div>
             </div>
