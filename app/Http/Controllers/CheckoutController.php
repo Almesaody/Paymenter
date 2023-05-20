fe6c38ce84c56be90422edd5b2d4885feddb35ab
Almesaody <hameralmesaody@gmail.com>
2023-05-20 12:04:36 +0300

    Updated virtualizor and rewritten config

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use App\Helpers\ExtensionHelper;
 use App\Models\{Orders, Products, User, Invoices, Extensions, Statistics};
 use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
-
 
 class CheckoutController extends Controller
 {
@@ -14,12 +14,12 @@ class CheckoutController extends Controller
         $products = session('cart');
         $total = 0;
 
-        if($products){
+        if ($products) {
             foreach ($products as $product) {
                 $total += $product->price * $product->quantity;
             }
         }
-        return view('checkout.index', compact('products', 'total'));	
+        return view('checkout.index', compact('products', 'total'));
     }
 
     public function success()
@@ -36,20 +36,21 @@ class CheckoutController extends Controller
     {
 
         $product = json_decode(Products::find($request->id)->toJson());
-        if(!$product) {
+        if (!$product) {
             return redirect()->back()->with('error', 'Product not found');
         }
         // Get extension config
-        $server = Extensions::find($product->server_id);
-        $extension = json_decode(file_get_contents(base_path('app/Extensions/Servers/' . $server->name . '/extension.json')));
-        
-        if(isset($extension->userConfig)){
-            return redirect()->route('checkout.config', $product->id);
+        if (isset($product->server_id)) {
+            $server = Extensions::find($product->server_id);
+            $extension = json_decode(file_get_contents(base_path('app/Extensions/Servers/' . $server->name . '/extension.json')));
+
+            if (isset($extension->userConfig)) {
+                return redirect()->route('checkout.config', $product->id);
+            }
         }
-        
         $product->quantity = 1;
         $cart = session()->get('cart');
-        if(\Illuminate\Support\Arr::has($cart, $product->id)) {
+        if (\Illuminate\Support\Arr::has($cart, $product->id)) {
             $cart[$product->id]->quantity++;
             session()->put('cart', $cart);
             return redirect()->back()->with('success', 'Product added to cart successfully!');
@@ -64,11 +65,13 @@ class CheckoutController extends Controller
     {
         $product = $id;
         $server = Extensions::find($product->server_id);
-        $extension = json_decode(file_get_contents(base_path('app/Extensions/Servers/' . $server->name . '/extension.json')));
-        if(!isset($extension->userConfig)){
+        include_once(base_path('app/Extensions/Servers/' . $server->name . '/index.php'));
+        $function = $server->name . '_getUserConfig';
+        $userConfig = json_decode(json_encode($function($product)));
+        if (!isset($userConfig)) {
             return redirect()->route('checkout.index');
         }
-        return view('checkout.config', compact('product', 'extension'));
+        return view('checkout.config', compact('product', 'userConfig'));
     }
 
     public function configPost(Request $request, Products $id)
@@ -83,7 +86,7 @@ class CheckoutController extends Controller
         $product->config = $config;
         $product->quantity = 1;
         $cart = session()->get('cart');
-        if(\Illuminate\Support\Arr::has($cart, $product->id)) {
+        if (\Illuminate\Support\Arr::has($cart, $product->id)) {
             $cart[$product->id]->quantity++;
             session()->put('cart', $cart);
             return redirect()->route('checkout.index')->with('success', 'Product added to cart successfully!');
@@ -97,11 +100,11 @@ class CheckoutController extends Controller
     public function pay(Request $request)
     {
         $products = session('cart');
-        if(!$products) {
+        if (!$products) {
             return redirect()->back()->with('error', 'Cart is empty');
         }
         $total = 0;
-        if(!$products) {
+        if (!$products) {
             return redirect()->back()->with('error', 'No products in cart');
         }
         if ($products) {
@@ -111,12 +114,11 @@ class CheckoutController extends Controller
         }
         $productsids = array();
         foreach ($products as $product) {
-            error_log(print_r($product, true));
             $productJson = [
                 'id' => $product->id,
                 'quantity' => $product->quantity,
             ];
-            if(isset($product->config)){
+            if (isset($product->config)) {
                 $productJson['config'] = $product->config;
             }
             array_push($productsids, $productJson);
@@ -127,21 +129,23 @@ class CheckoutController extends Controller
         $order->total = $total;
         $order->products = $productsids;
         $order->expiry_date = date('Y-m-d H:i:s', strtotime('+1 month'));
-        $order->status = 'pending';
+        if ($total == 0) {
+            $order->status = 'paid';
+            ExtensionHelper::createServer($order);
+        } else {
+            $order->status = 'pending';
+        }
         $order->save();
 
         $invoice = new Invoices();
         $invoice->user_id = $user->id;
         $invoice->order_id = $order->id;
-        $invoice->status = 'pending';
+        if ($total == 0) {
+            $invoice->status = 'paid';
+        } else {
+            $invoice->status = 'pending';
+        }
         $invoice->save();
-
-        Statistics::updateOrCreate(
-            [
-                'name' => 'orders',
-                'date' => date('Y-m-d'),
-            ]
-        )->increment('value');
 
         session()->forget('cart');
         return redirect()->route('invoice.show', ['id' => $invoice->id]);
@@ -150,7 +154,7 @@ class CheckoutController extends Controller
     public function remove(Request $request, $id)
     {
         $cart = session()->get('cart');
-        if(isset($cart[$id])) {
+        if (isset($cart[$id])) {
             unset($cart[$id]);
             session()->put('cart', $cart);
         }
@@ -166,7 +170,7 @@ class CheckoutController extends Controller
             'quantity' => 'required|numeric|min:1'
         ]);
         $cart = session()->get('cart');
-        if(isset($cart[$id])) {
+        if (isset($cart[$id])) {
             $cart[$id]->quantity = $request->quantity;
             session()->put('cart', $cart);
         }
