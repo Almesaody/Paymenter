9b7d133f77e09722a4503b054712da471f2514b2
Almesaody <120131207+Almesaody@users.noreply.github.com>
2023-06-28 13:30:53 +0300

    fix: fixed some bugs with proxmox + added lxc

--- app/Models/OrderProducts.php
+++ app/Models/OrderProducts.php
@@ -2,21 +2,49 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class OrderProducts extends Model
+class OrderProduct extends Model
 {
     use HasFactory;
     protected $table = 'order_products';
     protected $fillable = [
         'order_id',
         'product_id',
-        'quantity'
+        'quantity',
+        'price',
+        'billing_cycle',
+        'expiry_date',
+        'status',
     ];
 
     public function config()
     {
-        return $this->hasMany(OrderProductsConfig::class, 'order_product_id', 'product_id');
+        return $this->hasMany(OrderProductConfig::class, 'order_product_id', 'id');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id', 'id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+    public function invoices()
+    {
+        return $this->hasOne(InvoiceItem::class, 'product_id', 'id')->get()->first() ? $this->hasOne(InvoiceItem::class, 'product_id', 'id')->get()->first()->invoice() : new Invoice();
+    }
+
+    public function getOpenInvoices()
+    {
+        return $this->invoices()->get()->filter(function ($invoice) {
+            if($invoice->total == 0)
+                return false;
+            return $invoice->status == 'pending';
+        });
     }
 }
