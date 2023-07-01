45c3635fb527ca671d4903a7381e7e2c8b2d29ca
Almesaody <120131207+Almesaody@users.noreply.github.com>
2023-07-01 06:46:59 +0300

    Another update for client area +start with product

--- themes/default/views/components/application-logo.blade.php
+++
@@ -1,5 +0,0 @@
-@if (config('settings::app_logo'))
-    <img src="{{ asset(config('settings::app_logo')) }}" alt="{{ config('settings::app_name') }}" class="w-10" />
-@else
-    <img src="/img/logo.png" alt="Paymenter" class="w-10">
-@endif
